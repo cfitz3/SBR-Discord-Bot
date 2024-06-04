c2f1abdcf763641221013b6e0145f3d5371b6533
Connor Fitzpatrick <connorgfitzpatrick@gmail.com>
2024-06-04 16:25:38 +0000

    Synced local db with main

--- events/onJoin.js
+++ events/onJoin.js
@@ -15,8 +15,18 @@ const welcomeEmbed = (member) => {
 module.exports = {
     name: "guildMemberAdd",
     async execute(member) {
-        const welcomeChannel = await member.guild.channels.cache.find(channel => channel.name === 'welcome');
-        await welcomeChannel.fetch();
-        welcomeChannel.send({ content: `Welcome <@${member.id}>!`, embeds: [welcomeEmbed(member)] });
+      console.log('guildMemberAdd event fired');  
+
+ try {
+              const welcomeChannelId = '1242864037792845875'; // replace with your channel ID
+        const welcomeChannel = await member.guild.channels.cache.get(welcomeChannelId);
+        if (!welcomeChannel) {
+            console.error(`Channel with ID ${welcomeChannelId} not found`);
+            return;
+        }
+            welcomeChannel.send({ content: `Welcome <@${member.id}>!`, embeds: [welcomeEmbed(member)] });
+        } catch (error) {
+            console.error(`Error in guildMemberAdd: ${error.message}`);
+        }
     }
 }
