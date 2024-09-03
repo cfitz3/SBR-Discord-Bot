568ab2d014972e041bc0d458844cd6756f8cb33f
cfitz3 <165530834+cfitz3@users.noreply.github.com>
2024-09-03 17:30:08 +0100

    added whitelist function

---
+++ src/interactions/slash/mod/smpWhitelist.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const config = require('../../../../config.json');
+const { getUUID } = require('../../../api/constants/mowojangAPI.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('whitelist')
+        .setDescription('Apply to join the SMP!')
+        .addStringOption(option =>
+            option.setName('username')
+                .setDescription('Your Minecraft username.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Why would you like to join?')
+                .setRequired(true)),
+        
+    async execute(interaction) {
+        try {
+            // Get the username and reason from the interaction options
+            const username = interaction.options.getString('username');
+            const reason = interaction.options.getString('reason');
+            const uuid = await getUUID(username);
+
+            const embed = new EmbedBuilder()
+                .setTitle('Whitelist Application')
+                .setDescription(`Username: \`${username}\`\n\nReason: \`${reason}\``);
+
+            const button1 = new ButtonBuilder()
+                .setCustomId('accept')
+                .setLabel('Accept')
+                .setStyle(ButtonStyle.Success);
+
+            const button2 = new ButtonBuilder()
+                .setCustomId('deny')
+                .setLabel('Deny')
+                .setStyle(ButtonStyle.Danger);
+
+            const row = new ActionRowBuilder()
+                .addComponents(button1, button2);
+
+            await interaction.reply({ embeds: [embed], components: [row] });
+
+            const filter = (interaction) => interaction.member.roles.cache.has(config.server.admin);
+            const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
+
+            collector.on('collect', async (interaction) => {
+                if (interaction.customId === 'accept') {
+                    await interaction.member.roles.add(config.server.whitelist_role);
+                    // Write username and uuid to testdatabase.json
+                    const data = [{
+                        uuid: uuid,
+                        name: username
+                    }];
+                    const filePath = path.join(__dirname, '../../../Modded-Server/whitelist.json');
+                    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+                } else if (interaction.customId === 'deny') {
+                    await interaction.reply('Your application has been denied.');
+                }
+            }); 
+
+            collector.on('end', () => {
+                // Collector ended
+            });
+
+        } catch (error) {  
+            console.log(error);
+        }
+    }
+};
